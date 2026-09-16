05e047dc33c63efc345738ee5f612fd5b057fbf9
medhasWorld <pradeepshopgroup@gmail.com>
2026-09-15 18:09:13 -0700

    Add files via upload

---
+++ Homework14.py
@@ -0,0 +1,10 @@
+a = input("Do u want the computer to shutdown? Yes/No: ")
+b = "Yes"
+c = "No"
+if a == b:
+    print("Shutting down...")
+else:
+        if a == c:
+              print("Abort shut down...")
+        else:
+                print("Sorry")
